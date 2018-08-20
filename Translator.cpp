219c1c1927d9b7dffb91ba5cef5220c7cbff4602
Jashan Kishore <jkishore@umich.edu>
2018-08-20 13:25:27 -0400

    Update Translator.cpp

--- Translator.cpp
+++ Translator.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <deque>
-#include <iomanip>
-#include <limits>
-#include <math.h>
-#include <queue>
-#include <fstream>
-
-using namespace std;
 
