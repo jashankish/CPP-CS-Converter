0fa4bafa105556c51ebab1e430fba003f8993614
Jashan Kishore <jkishore@umich.edu>
2018-08-20 13:23:20 -0400

    Create Translator.cpp

---
+++ Translator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <iomanip>
+#include <limits>
+#include <math.h>
+#include <queue>
+#include <fstream>
+
+using namespace std;
+
